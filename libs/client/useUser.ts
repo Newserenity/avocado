1e53260b8f3c81c1678cbf07add73ca64410c5bb
Jayden Jang <jaydenjang@Jaydens-MacBook-Air.local>
2022-12-29 23:45:12 +0900

    useUser hook with axios

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,21 +1,28 @@
 import axios from 'axios'
 import { Router, useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import useSWR from 'swr'
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+const fetcher = (url: string) => axios(url).then((res) => res.data)
 
 export default function useUser() {
-  const [user, setUser] = useState()
-  const router = useRouter()
-  useEffect(() => {
-    // axios.get('/api/users/me').then((res) => console.log(res.data))
-    fetch('/api/users/me')
-      .then((res) => res.json())
-      .then((json) => {
-        if (!json.ok) {
-          return router.replace('/users/login')
-        }
-        setUser(json.profile)
-      })
-  }, [router])
+  const { data, error } = useSWR('/api/users/me', fetcher)
 
-  return user
+  // const [user, setUser] = useState()
+  //   const router = useRouter()
+  //   useEffect(() => {
+  //     // axios.get('/api/users/me').then((res) => console.log(res.data))
+  //     fetch('/api/users/me')
+  //       .then((res) => res.json())
+  //       .then((json) => {
+  //         if (!json.ok) {
+  //           return router.replace('/users/login')
+  //         }
+  //         setUser(json.profile)
+  //       })
+  //   }, [router])
+
+  return data
 }
